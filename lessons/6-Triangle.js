b8e6c6327d3f5b07ff1a8bac4542f9790edb8698
Konstantin Gusev <guvkon@gmail.com>
2018-12-06 18:42:56 +0700

    Add 100% solution for 6-Triangle

--- lessons/6-Triangle.js
+++ lessons/6-Triangle.js
@@ -1,44 +1,16 @@
 function solution(A) {
     A.sort((a, b) => a - b);
-    const uniques = [];
-    const counter = {};
-    for (let i = 0; i < A.length; i++) {
-        const a = A[i];
-        if (counter[a] === undefined) {
-            counter[a] = 1;
-            uniques.push(a);
-        } else {
-            counter[a]++;
+
+    let result = 0;
+    for (let i = 0; i < A.length - 2; i++) {
+        if (isTriangle(A[i], A[i+1], A[i+2])) {
+            result = 1;
+            break;
         }
     }
-    return hasTriangular(uniques, counter) ? 1 : 0;
+    return result;
 }
 
-function hasTriangular(uniques, counter) {
-    for (let i = 0; i < uniques.length - 1; i++) {
-        if (counter[uniques[i]] > 1) {
-            if (isTriangular(uniques[i], uniques[i], uniques[i+1])) {
-                return true;
-            }
-        }
-        if (counter[uniques[i + 1]] > 1) {
-            if (isTriangular(uniques[i], uniques[i+1], uniques[i+1])) {
-                return true;
-            }
-        }
-        if (i < uniques.length - 2) {
-            if (isTriangular(uniques[i], uniques[i+1], uniques[i+2])) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-function isTriangular(a, b, c) {
-    return (
-        (a + b > c) &&
-        (c + a > b) &&
-        (b + c > a)
-    );
+const isTriangle = (a, b, c) => {
+    return a + b > c && c + a > b && b + c > a;
 }
