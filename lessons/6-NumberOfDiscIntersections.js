c22bf9683e2959e4a01bef05a337422cc53c04ef
Konstantin Gusev <guvkon@gmail.com>
2018-12-07 15:31:34 +0700

    Add 100% solution for 6-NumberOfDiscIntersections

--- lessons/6-NumberOfDiscIntersections.js
+++ lessons/6-NumberOfDiscIntersections.js
@@ -1,14 +1,36 @@
 function solution(A) {
-    let count = 0;
-    for (let c1 = 0; c1 < A.length; c1++) {
-        const r1 = A[c1];
-        for (let c2 = c1 + 1; c2 < A.length; c2++) {
-            const r2 = A[c2];
-            if (c1 + r1 >= c2 - r2) {
-                count++;
+    const lines = Array(A.length);
+    for (let i = 0; i < lines.length; i++) {
+        lines[i] = [];
+    }
+
+    for (let center = 0; center < A.length; center++) {
+        const radius = A[center];
+        let left = center - radius;
+        let length = 2 * radius;
+        if (left < 0) {
+            length += left;
+            left = 0;
+        }
+        lines[left].push(length);
+    }
+
+    let counter = 0;
+    for (let left = 0; left < lines.length; left++) {
+        for (let j = 0; j < lines[left].length; j++) {
+            const length = lines[left][j];
+            counter += lines[left].length - 1 - j; // calculate number of unordered pairs on the same spot
+            if (counter > 10000000) {
+                return -1;
+            }
+            for (let k = left + 1; k <= left + length && k < lines.length; k++) {
+                counter += lines[k].length;
+                if (counter > 10000000) {
+                    return -1;
+                }
             }
         }
     }
-    return count;
+    return counter;
 }
 
