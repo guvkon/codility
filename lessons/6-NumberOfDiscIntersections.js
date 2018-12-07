9b97909350909bd8c568f84d07340c10ccf274bd
Konstantin Gusev <guvkon@gmail.com>
2018-12-07 12:07:39 +0700

    Add smaller solution for 6-NumberOfDiscIntersections

--- lessons/6-NumberOfDiscIntersections.js
+++ lessons/6-NumberOfDiscIntersections.js
@@ -1,31 +1,14 @@
 function solution(A) {
-    const isPointInside = (center, radius, point) => {
-        return point >= center - radius && point <= center + radius;
-    }
-    
-    const circlesAtPoint = Array(A.length);
-    for (let i = 0; i < A.length; i++) {
-        circlesAtPoint[i] = [];
-        for (let j = 0; j < A.length; j++) {
-            if (isPointInside(j, A[j], i)) {
-                circlesAtPoint[i].push(j);
-            }
-        }
-    }
-    
-    const intercepts = {};
-    for (let i = 0; i < circlesAtPoint.length; i++) {
-        for (let j = 0; j < circlesAtPoint[i].length; j++) {
-            for (let k = 0; k < circlesAtPoint[i].length && k !== j; k++) {
-                const pair = String(circlesAtPoint[i][j]) + '-' + String(circlesAtPoint[i][k]);
-                intercepts[pair] = true;
-            }
-        }
-    }
-    
     let count = 0;
-    for (let key in intercepts) {
-        count++;
+    for (let c1 = 0; c1 < A.length; c1++) {
+        const r1 = A[c1];
+        for (let c2 = c1 + 1; c2 < A.length; c2++) {
+            const r2 = A[c2];
+            if (c1 + r1 >= c2 - r2) {
+                count++;
+            }
+        }
     }
     return count;
 }
+
